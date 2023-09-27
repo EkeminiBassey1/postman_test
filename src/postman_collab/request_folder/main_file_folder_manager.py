e5444e53c990783163e9b0e78315d9afe1a81fa2
EkeminiBassey1 <100595429+EkeminiBassey1@users.noreply.github.com>
2023-09-27 16:03:09 +0200

    correcting imports

--- src/postman_collab/request_folder/main_file_folder_manager.py
+++ src/postman_collab/request_folder/main_file_folder_manager.py
@@ -1,4 +1,4 @@
-from ..request_folder.class_file_folder_manager import FolderAndFileManager
+from src.postman_collab.request_folder.class_file_folder_manager import FolderAndFileManager
 folder_file_manager = FolderAndFileManager()
 
 data_dict = folder_file_manager.get_api_request_file()

c4825bd543bed4eed5e9b89003f3a27680ce391b
EkeminiBassey1 <100595429+EkeminiBassey1@users.noreply.github.com>
2023-09-27 17:31:06 +0200

    correcting imports

--- class_file_folder_manager.py
+++ class_file_folder_manager.py
@@ -1,5 +1,4 @@
 import json
-import os
 import importlib.resources
 import utils.api_json as file_api
 
@@ -10,20 +9,9 @@ class FolderAndFileManager:
 
     def get_api_request_file(self):
         json_data = importlib.resources.read_text(file_api, 'api_response.json')
-
-        print(json_data)
-
         data_dict = json.loads(json_data)
         return data_dict
 
     def write_into_file(self, file_name: str, content: str):
         with open(file_name, 'w') as file:
             json.dump(content, file)
-
-    def check_if_file_exists(self):
-        if not os.path.exists(self.folder_path):
-            os.makedirs(self.folder_path)
-            os.chdir(self.folder_path)
-        else:
-            os.chdir(self.folder_path)
-            print(f"The directory '{self.folder_path}' already exists.")

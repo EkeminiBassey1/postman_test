afe1d4f46c0b33d13057dd372f55b82d6e7184fe
EkeminiBassey1 <100595429+EkeminiBassey1@users.noreply.github.com>
2023-09-28 09:22:53 +0200

    remove check if folder is being created

--- main_file_folder_manager.py
+++ main_file_folder_manager.py
@@ -3,7 +3,6 @@ from class_file_folder_manager import FolderAndFileManager
 folder_file_manager = FolderAndFileManager()
 
 data_dict = folder_file_manager.get_api_request_file()
-folder_file_manager.check_if_file_exists()
 
 for item in data_dict['collection']['item']:
     file_name = str(item['id']) + ".json"

12850a15a6bd47f6a3bbfcd490b67d2ff700bb0d
EkeminiBassey1 <100595429+EkeminiBassey1@users.noreply.github.com>
2023-09-28 14:36:06 +0200

    Execute the python file

--- class_file_folder_manager.py
+++ class_file_folder_manager.py
@@ -1,13 +1,11 @@
 import json
-import importlib.resources
-import utils.api_json as file_api
 
 
 class FolderAndFileManager:
     def get_api_request_file(self):
-        json_data = importlib.resources.read_text(file_api, 'api_response.json')
-        data_dict = json.loads(json_data)
-        return data_dict
+        with open("api_response.json", 'r') as file:
+            json_content = json.load(file)
+        return json_content
 
     def write_into_file(self, file_name: str, content: str):
         with open(file_name, 'w') as file:

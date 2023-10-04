23e7c84849c2795c928500c02cac27dbe7af8955
EkeminiBassey1 <100595429+EkeminiBassey1@users.noreply.github.com>
2023-10-04 14:37:18 +0200

    Check if there has been changes

--- class_file_folder_manager.py
+++ class_file_folder_manager.py
@@ -1,14 +1,9 @@
 import json
 import os
-import postman_collections as file_collection
-import importlib.resources
 
 
 class FolderAndFileManager:
     def get_api_request_file(self):
-        data = json.load(importlib.resources.read_text(file_collection, "collection_sync.json"))
-        print(data)
-
         with open("postman_collections/collection_sync.json", 'r') as file:
             json_content = json.load(file)
         return json_content
